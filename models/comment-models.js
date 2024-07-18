ea58cbef44be606cb397c12f8e670e13064baaf8
SADAF MTAHERI <sadaf@SADAFs-MacBook-Pro.local>
2024-07-18 14:36:35 +0100

    task number 10 done

--- models/comment-models.js
+++ models/comment-models.js
@@ -1,7 +1,17 @@
 const db = require("../db/connection");
 
 exports.removeCommentId = (comment_id) => {
-  return db.query("DELETE FROM comments WHERE comment_id = $1 RETURNING *;", [
-    comment_id,
-  ]);
+  return db
+    .query("DELETE FROM comments WHERE comment_id = $1 RETURNING *;", [
+      comment_id,
+    ])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: `No Comment found under ${comment_id}`,
+        });
+      }
+      return rows[0];
+    });
 };
